72f668681bf02ba57d0d5e9ac5a8c3a9b23caa17
indecisivenitin <ahir6346@gmail.com>
2026-02-25 01:03:06 +0530

    update app cors - added vercel live url

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: ["http://localhost:5173",
-            "https://your-frontend-url.vercel.app"
+            "https://dev-flow-ai-six.vercel.app/"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
